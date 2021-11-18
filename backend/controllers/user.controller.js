5d3823a2a6d4d9596ed908a0109ea912fd77db0e
Mohamed Testouri <mohamed.testouri@esprit.tn>
2021-11-18 15:34:09 +0100

    module user added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,66 +1,176 @@
-const { hash } = require('bcrypt');
-const express = require('express');
-const router = express.Router();
 const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const nodemailer = require('nodemailer');
+const mailer = require('nodemailer');
 const User = require('../models/User');
 
+const transporter = mailer.createTransport({
+    service: 'gmail',
+    secure: false,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.MAIL_PASSWORD
+    },
+    tls: {
+        // do not fail on invalid certs
+        rejectUnauthorized: false
+    },
+});
+function sendmail(mailOptions) {
+    transporter.sendMail(mailOptions, async function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
 exports.signup = (req, res) => {
-  User.find({ email: req.body.email })
-    .exec()
-    .then((user) => {
-      if (user.email === req.body.email) {
-        return res.status(409).json({ error: 'Mail Exists' });
-      } else {
-        bcrypt.hash(req.body.password, 10, (error, hash) => {
-          if (error) {
-            return res.status(500).json({ error: error });
-          } else {
-            const user = new User({
-              _id: new mongoose.Types.ObjectId(),
-              ...req.body,
-            });
-            const token = jwt.sign(
-              {
-                email: user.email,
-                password: user.password,
-              },
-              process.env.SECRET_KEY,
-              { expiresIn: '24h' }
-            );
-            const data = {
-              from: 'SkouShop Team <no-reply@skou-shop.tn>',
-              to: req.body.email,
-              subject: 'Account Activation',
-              html: `<h2> Please click on given button to activate you account </h2>
-                      <a href=" http://localhost:6000/activation/${token}" > Verify your account</a>`,
-            };
-            user
-              .save()
-              .then((result) => {
-                console.log(result);
-                mg.messages().send(data, function (error, body) {
-                  if (error) {
-                    return res.json({
-                      error: error,
-                    });
-                  } else {
-                    return res.status(200).json({
-                      user: result,
-                    });
-                  }
+    User.find({email: req.body.email})
+        .exec()
+        .then((users) => {
+            console.log(users)
+            if (users.length >= 1) {
+                console.log("true")
+                return res.status(409).json({error: 'Mail Exists'});
+            } else {
+                bcrypt.hash(req.body.password, 10, async (error, hash) => {
+                    if (error) {
+                        return res.status(500).json(error);
+                    } else {
+                        const resetCode = Math.floor(Math.random() * 9999);
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash,
+                            phone: req.body.phone,
+                            name: req.body.name,
+                            resetCode: resetCode
+                        });
+                        let mailOptions = {
+                            from: 'Aikido.ClubT@gmail.com',
+                            to: req.body.email,
+                            subject: 'Verify your Account',
+                            text: 'Here is your verification code: ' + resetCode
+                        };
+                        sendmail(mailOptions);
+                        await user.save();
+                        return res.status(200).json(user);
+                    }
                 });
-              })
-              .catch((error) => {
-                return res.status(500).json({ error: error });
-              });
-          }
+            }
         });
-      }
+};
+exports.login = async (req, res) => {
+    const user = await User.findOne({email: req.body.email});
+    if (!user) {
+        return res.status(404).json({message: 'E-mail does not exist'});
+    }
+    const verifyPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!verifyPassword) {
+        return res.status(400).send({message: 'Password is invalid'});
+    }
+    if (!user.isActive) {
+        return res.status(400).send({message: 'Account is disabled'});
+    }
+    return await res.status(200).json(user);
+};
+exports.forgetPassword = async (req, res) => {
+    const emailExist = await User.findOne({email: req.body.email});
+    if (!emailExist) {
+        return res.status(400).json({message: 'Email does not exist'});
+    }
+    const resetCode = Math.floor(Math.random() * 9999);
+    User.findOneAndUpdate({email: req.body.email}, {$set: {resetCode: resetCode}}, function (error, user) {
+        if (error) {
+            return res.status(error.code).json(error);
+        } else {
+            let mailOptions = {
+                from: 'Aikido.ClubT@gmail.com',
+                to: req.body.email,
+                subject: 'Reset Password',
+                text: 'Here is your verification code: ' + resetCode
+            };
+            sendmail(mailOptions);
+            return res.status(200).json({message: "Reset code sent successfully", user});
+        }
     });
 };
+exports.activateAccount = async (req, res) => {
+    const code = req.body.code;
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return res.status(400).json({ message: 'E-mail does not exist' });
+    }
+    if (user.resetCode === code) {
+        User.findOneAndUpdate({ email: req.body.email }, { $set: { isActive: true } }, function (error, user) {
+            if (error) {
+                return res.status(error.code).json(error);
+            }
+            return res.status(200).json(user);
+        });
+    } else {
+        return res.status(400).json({ message: "Invalid verification code" });
+    }
+};
+exports.resetPassword = async (req, res) => {
+    const user = await User.findOne({email: req.body.email});
+    if (!user) {
+        return res.status(400).json({message: 'Email does not exist'});
+    }
+    if (user.resetCode === req.body.resetCode) {
+        User.findOneAndUpdate({email: req.body.email}, {$set: {password: bcrypt.hashSync(req.body.password, 10)}}, function (error, user) {
+            if (error) {
+                return res.status(error.code).json(error);
+            }
+            return res.status(200).json({message: "Password updated successfully", user});
+        });
+    } else {
+        return res.status(400).json({message: "Invalid credentials!"});
+    }
+};
+exports.updatePassword = async (req, res) => {
+    const user = await User.findOne({email: req.body.email});
+    if (!user) {
+        return res.status(400).json({message: 'Email does not exist'});
+    }
+    const verifyPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!verifyPassword) {
+        return res.status(400).send({message: 'Password is invalid'});
+    } else {
+        User.findOneAndUpdate({email: req.body.email}, {$set: {password: bcrypt.hashSync(req.body.repassword, 10)}}, function (error, user) {
+            if (error) {
+                return res.status(error.code).json(error);
+            }
+            return res.status(200).json({message: "Password updated successfully", user});
+        });
+    }
+};
+exports.deleteAccount = (req, res) => {
+    const email = req.body.email;
+    User.remove({email: email})
+        .exec()
+        .then((result) => {
+            return res.status(200).json(result);
+        })
+        .catch((error) => {
+            return res.status(error.code).json({error: error});
+        });
+};
+exports.changeRole = async (req, res) => {
+    const user = await User.findOne({email: req.body.email});
+    if (!user) {
+        return res.status(404).json({message: 'E-mail does not exist'});
+    }
+    User.findOneAndUpdate({email: req.body.email}, {$set: {isAdmin: !user.isAdmin}}, function (error, user) {
+        if (error) {
+            return res.status(error.code).json(error);
+        }
+        return res.status(200).json({message: "Password updated successfully", user});
+    });
+};
+
+
 /*const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
--- backend/models/User.js
+++ backend/models/User.js
@@ -16,6 +16,7 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+  resetCode: Number,
   phone: Number,
   isActive: { type:Boolean, default: false},
   isAdmin: { type: Boolean, default: false },
